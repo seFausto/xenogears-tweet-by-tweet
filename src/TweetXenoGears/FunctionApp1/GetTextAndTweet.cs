22004e2eee5f5ce846a93aedfb4f8b3cc3ff8999
Fausto Ruvalcaba <se.fausto@outlook.com>
2022-09-06 15:05:24 -0700

    More async

--- src/TweetXenoGears/FunctionApp1/GetTextAndTweet.cs
+++ src/TweetXenoGears/FunctionApp1/GetTextAndTweet.cs
@@ -15,7 +15,7 @@ namespace XenoGearsByTweet
         private const string DatabaseName = "xeno.sqlite";
         private const string Insertcommand = "insert into script values (@line, @count)";
         private const string CreateTableCommand = "create table script (line varchar(8000), number int)";
-        private static int nextLineIndex = 0;
+        private static int NextLineIndex = 0;
 
         [FunctionName("TweetXenoGears")]
         public static async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log)
@@ -25,11 +25,11 @@ namespace XenoGearsByTweet
                 if (!File.Exists(DatabaseName))
                     await CreateDatabase();
 
-                string line = await GetNextLineAsync(nextLineIndex);
+                string line = await GetNextLineAsync(NextLineIndex);
 
-                log.LogInformation("Tweeting line #{Index}: {Line}", nextLineIndex, line);
+                log.LogInformation("Tweeting line #{Index}: {Line}", NextLineIndex, line);
 
-                nextLineIndex++;
+                NextLineIndex++;
 
             }
             catch (Exception ex)
@@ -56,7 +56,7 @@ namespace XenoGearsByTweet
 
         private static async Task CreateDatabase()
         {
-            SQLiteConnection db = CreateAndGetDb();
+            SQLiteConnection db = await CreateAndGetDb();
 
             await AddLinesToDb(db);
         }
@@ -77,13 +77,16 @@ namespace XenoGearsByTweet
                 {
                     var line = await reader.ReadLineAsync();
                     string insertQuery = Insertcommand;
+                    await db.OpenAsync();
 
                     SQLiteCommand command2 = new(insertQuery, db);
                     command2.Parameters.AddWithValue("line", line);
                     command2.Parameters.AddWithValue("count", count);
                     command2.ExecuteNonQuery();
-
                     count++;
+
+                    await db.CloseAsync();
+                    
                 }
                 catch (Exception ex)
                 {
@@ -93,18 +96,20 @@ namespace XenoGearsByTweet
             }
         }
 
-        private static SQLiteConnection CreateAndGetDb()
+        private static async Task<SQLiteConnection> CreateAndGetDb()
         {
             SQLiteConnection.CreateFile(DatabaseName);
 
             var db = new SQLiteConnection($"Data Source={DatabaseName}");
-            db.Open();
+            await db.OpenAsync();
 
             string sql = CreateTableCommand;
 
             SQLiteCommand command = new(sql, db);
 
-            command.ExecuteNonQuery();
+            await command.ExecuteNonQueryAsync();
+            
+            await db.CloseAsync();
 
             return db;
         }
