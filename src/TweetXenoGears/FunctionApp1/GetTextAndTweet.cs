fe4b3dd686608e09bddf9befc59aeec328905c33
Fausto Ruvalcaba <se.fausto@outlook.com>
2022-09-08 07:17:07 -0700

    Add env variable for sqlite db

--- src/TweetXenoGears/FunctionApp1/GetTextAndTweet.cs
+++ src/TweetXenoGears/FunctionApp1/GetTextAndTweet.cs
@@ -14,29 +14,37 @@ namespace XenoGearsByTweet
 {
     public class GetTextAndTweet
     {
-        private const string DatabaseName = "xeno.sqlite";
-        private const string Insertcommand = "insert into script values (@line, @count)";
-        private const string CreateTableCommand = "create table script (line varchar(8000), number int)";
-        private const string CreateIndexTableCommand = "create table IndexTable (number int)";
+        private static string DatabaseName = "xeno.sqlite";
+
+        private const string Insertcommand = "INSERT INTO script VALUES (@line, @count)";
+        private const string CreateTableCommand = "CREATE TABLE script (line VARCHAR(8000), number INT)";
+        private const string CreateIndexTableCommand = "CREATE TABLE IndexTable (number INT)";
+        private const string NextIndexQuery = @"
+                    select max(number) from IndexTable;
+                    insert into IndexTable values ((select max(number) from IndexTable) + 1);
+                ";
+
 
         [FunctionName("TweetXenoGears")]
-        public static async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 */10 * * * *")] TimerInfo myTimer, ILogger log)
         {
+            DatabaseName = Environment.GetEnvironmentVariable("DatabasePath");
+
             try
             {
                 if (!File.Exists(DatabaseName))
                     await CreateDatabase();
 
-                int nextLineIndex = await GetNextLineIndexAsync();
-                log.LogInformation("Getting next line with index {Index}", nextLineIndex);
-                string line = string.Empty;
-                line = await GetNextLineAsync(nextLineIndex);
+                log.LogInformation("Getting index at {Timer}", myTimer);
+                int nextLineIndex = await GetNextLineIndexAsync(log);
 
-                log.LogInformation("Tweeting line #{Index}: {Line}", nextLineIndex, line);
+                log.LogInformation("Getting next line with index {Index} at {Timer}", nextLineIndex, myTimer);
+                string line = await GetNextLineAsync(nextLineIndex);
+
+                log.LogInformation("Tweeting line #{Index}: {Line} at {Timer}", nextLineIndex, line, myTimer);
                 var twitterBusiness = new TwitterBusiness();
 
                 await twitterBusiness.TweetStringList(line);
-
             }
             catch (Exception ex)
             {
@@ -44,24 +52,21 @@ namespace XenoGearsByTweet
             }
         }
 
-        private static async Task<int> GetNextLineIndexAsync()
+        private static async Task<int> GetNextLineIndexAsync(ILogger log)
         {
-            const string query = @"
-                    select max(number) from IndexTable;
-                    insert into IndexTable values ((select max(number) from IndexTable) + 1);
-                ";
             try
             {
+                log.LogDebug("Creating Database");
                 using var db = new SQLiteConnection($"Data Source={DatabaseName}");
                 await db.OpenAsync();
 
-                SQLiteCommand command2 = new(query, db);
+                SQLiteCommand command2 = new(NextIndexQuery, db);
                 var result = await command2.ExecuteScalarAsync();
                 await db.CloseAsync();
                 return Convert.ToInt32(result);
 
             }
-            catch (Exception ex)
+            catch
             {
                 throw;
             }
@@ -97,7 +102,6 @@ namespace XenoGearsByTweet
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
@@ -106,17 +110,17 @@ namespace XenoGearsByTweet
         {
             try
             {
-
                 string insertQuery = "insert into IndexTable (number) values (0);";
 
                 await db.OpenAsync();
 
                 SQLiteCommand command2 = new(insertQuery, db);
-                await command2.ExecuteNonQueryAsync();
-                await db.CloseAsync();
 
+                await command2.ExecuteNonQueryAsync();
+
+                await db.CloseAsync();
             }
-            catch (Exception ex)
+            catch
             {
                 throw;
             }
@@ -125,6 +129,7 @@ namespace XenoGearsByTweet
         private static async Task InsertScriptTable(SQLiteConnection db)
         {
             var assembly = Assembly.GetExecutingAssembly();
+
             string resourceName = assembly.GetManifestResourceNames()
                 .Single(str => str.EndsWith("xenogears-disc1.txt"));
 
@@ -147,9 +152,8 @@ namespace XenoGearsByTweet
                     count++;
 
                     await db.CloseAsync();
-
                 }
-                catch (Exception ex)
+                catch
                 {
                     throw;
                 }
@@ -159,6 +163,7 @@ namespace XenoGearsByTweet
 
         private static async Task<SQLiteConnection> CreateAndGetDb()
         {
+
             SQLiteConnection.CreateFile(DatabaseName);
 
             var db = new SQLiteConnection($"Data Source={DatabaseName}");
