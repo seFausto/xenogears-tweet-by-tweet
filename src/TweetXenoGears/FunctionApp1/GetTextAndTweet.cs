23773f4c8b08c981659bc13baf0a450849eab945
Fausto Ruvalcaba <se.fausto@outlook.com>
2022-09-07 21:26:38 -0700

    Add Twitter business

--- src/TweetXenoGears/FunctionApp1/GetTextAndTweet.cs
+++ src/TweetXenoGears/FunctionApp1/GetTextAndTweet.cs
@@ -4,6 +4,7 @@ using System.Data.SQLite;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -15,53 +16,111 @@ namespace XenoGearsByTweet
         private const string DatabaseName = "xeno.sqlite";
         private const string Insertcommand = "insert into script values (@line, @count)";
         private const string CreateTableCommand = "create table script (line varchar(8000), number int)";
-        private static int NextLineIndex = 0;
+        private const string CreateIndexTableCommand = "create table IndexTable (number int)";
 
         [FunctionName("TweetXenoGears")]
         public static async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log)
         {
+            string line = string.Empty;
+
             try
             {
                 if (!File.Exists(DatabaseName))
                     await CreateDatabase();
 
-                string line = await GetNextLineAsync(NextLineIndex);
+                int nextLineIndex = await GetNextLineIndexAsync();
 
-                log.LogInformation("Tweeting line #{Index}: {Line}", NextLineIndex, line);
+                line = await GetNextLineAsync(nextLineIndex);
 
-                NextLineIndex++;
+                log.LogInformation("Tweeting line #{Index}: {Line}", nextLineIndex, line);
 
             }
             catch (Exception ex)
             {
                 log.LogWarning(ex, "Warning thrown");
             }
+        }
 
-            log.LogDebug($"C# Timer trigger function executed at: {DateTime.Now}");
+        private static async Task<int> GetNextLineIndexAsync()
+        {
+            const string query = @"
+                    select max(number) from IndexTable;
+                    insert into IndexTable values ((select max(number) from IndexTable) + 1);
+                ";
+            try
+            {
+                using var db = new SQLiteConnection($"Data Source={DatabaseName}");
+                await db.OpenAsync();
+
+                SQLiteCommand command2 = new(query, db);
+                var result = await command2.ExecuteScalarAsync();
+                await db.CloseAsync();
+                return Convert.ToInt32(result);
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
         }
 
         private static async Task<string> GetNextLineAsync(int index)
         {
+            const string sql = "SELECT line FROM script WHERE number = @index";
+
             using var db = new SQLiteConnection($"Data Source={DatabaseName}");
 
-            db.Open();
-
-            string sql = "SELECT line FROM script WHERE number = @index";
+            await db.OpenAsync();
 
             SQLiteCommand command = new(sql, db);
             command.Parameters.AddWithValue("index", index);
 
-            return Convert.ToString(await command.ExecuteScalarAsync());
+            object value = await command.ExecuteScalarAsync();
+            await db.CloseAsync();
+
+            return Convert.ToString(value);
         }
 
         private static async Task CreateDatabase()
         {
-            SQLiteConnection db = await CreateAndGetDb();
+            try
+            {
+                SQLiteConnection db = await CreateAndGetDb();
 
-            await AddLinesToDb(db);
+                await InsertScriptTable(db);
+
+                await InsertIndexTable(db);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
-        private static async Task AddLinesToDb(SQLiteConnection db)
+        private static async Task InsertIndexTable(SQLiteConnection db)
+        {
+            try
+            {
+
+                string insertQuery = "insert into IndexTable values (0);";
+
+                await db.OpenAsync();
+
+                SQLiteCommand command2 = new(insertQuery, db);
+
+
+                await db.CloseAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private static async Task InsertScriptTable(SQLiteConnection db)
         {
             var assembly = Assembly.GetExecutingAssembly();
             string resourceName = assembly.GetManifestResourceNames()
@@ -86,7 +145,7 @@ namespace XenoGearsByTweet
                     count++;
 
                     await db.CloseAsync();
-                    
+
                 }
                 catch (Exception ex)
                 {
@@ -103,12 +162,14 @@ namespace XenoGearsByTweet
             var db = new SQLiteConnection($"Data Source={DatabaseName}");
             await db.OpenAsync();
 
-            string sql = CreateTableCommand;
-
-            SQLiteCommand command = new(sql, db);
+            SQLiteCommand command = new(CreateTableCommand, db);
 
             await command.ExecuteNonQueryAsync();
-            
+
+            command = new(CreateIndexTableCommand, db);
+
+            await command.ExecuteNonQueryAsync();
+
             await db.CloseAsync();
 
             return db;
---
+++ src/TweetXenoGears/FunctionApp1/TwitterBusiness.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Parameters;
+
+namespace FunctionApp1
+{
+
+    class TwitterSettings
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string ApiKeySecret { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string AccessTokenSecret { get; set; } = string.Empty;
+        public long UserId { get; set; }
+    }
+
+    public class TwitterBusiness
+    {
+        private readonly TwitterSettings _twitterSettings;
+
+        public TwitterBusiness()
+        {
+            _twitterSettings = new TwitterSettings
+            {
+                ApiKey = Environment.GetEnvironmentVariable("ApiKey"),
+                ApiKeySecret = Environment.GetEnvironmentVariable("ApiKeySecret"),
+                AccessToken = Environment.GetEnvironmentVariable("AccessToken"),
+                AccessTokenSecret = Environment.GetEnvironmentVariable("AccessTokenSecret"),
+                UserId = Convert.ToInt64(Environment.GetEnvironmentVariable("UserId"))
+            };
+        }
+
+        public async Task TweetStringList(Tweetinvi.Models.ITweet tweet, string message)
+        {
+            var userClient = GetTwitterClient();
+            try
+            {
+                // Log.Information("Quoting Tweet {TweetId} from {ScreenName}: {Message}", tweet.Id,
+                //    tweet.CreatedBy.ScreenName, message);
+
+                _ = await userClient.Tweets.PublishTweetAsync(new PublishTweetParameters(message)
+                {
+                    QuotedTweet = tweet
+                });
+            }
+            catch (Exception ex)
+            {
+                // Log.Error(ex, "Error quoting tweet {TweetId}", tweet.Id);
+                throw;
+            }
+        }
+
+        private TwitterClient GetTwitterClient()
+        {
+            return new TwitterClient(_twitterSettings.ApiKey, _twitterSettings.ApiKeySecret,
+                _twitterSettings.AccessToken, _twitterSettings.AccessTokenSecret);
+        }
+    }
+}
